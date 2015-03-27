600dc4c8b2e2366d20701ce9113090ef2bc237f7
Atul Manwar <atul@swiggy.com>
2015-03-28 00:33:04 +0530

    Initial Commit

---
+++ ChatterBuzzTests/ChatterBuzzTests.swift
@@ -0,0 +1,36 @@
+//
+//  ChatterBuzzTests.swift
+//  ChatterBuzzTests
+//
+//  Created by Atul Manwar on 28/03/15.
+//  Copyright (c) 2015 sfm. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class ChatterBuzzTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
