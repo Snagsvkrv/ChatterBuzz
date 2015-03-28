a2990e04465977b2742115d44920e9f22dac5d8d
Atul Manwar <atul@swiggy.com>
2015-03-28 15:43:09 +0530

    Chatterbuzz new code

--- ChatterBuzz/BhiBhi.m
+++
@@ -1,9 +0,0 @@
-//
-//  BhiBhi.m
-//  ChatterBuzz
-//
-//  Created by Atul Manwar on 28/03/15.
-//  Copyright (c) 2015 sfm. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
--- README.md
+++
@@ -1,2 +0,0 @@
-# ChatterBuzz
-Hikeathon
